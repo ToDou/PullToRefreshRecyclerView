401cf2f0711fd348b8277b7167060582d91e1cd0
ToDou <yangyu@loopeer.com>
2015-07-10 18:18:31 +0800

    fix the pull to refresh

--- app/src/main/java/com/todou/pulltorefreshrecyclerview/MainActivity.java
+++ app/src/main/java/com/todou/pulltorefreshrecyclerview/MainActivity.java
@@ -2,23 +2,26 @@ package com.todou.pulltorefreshrecyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
-import com.loopeer.pulltorefreshrecyclerview.view.PullToRefreshRecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import com.todou.pulltorefreshrecyclerview.adapter.MainAdapter;
 
 import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 
 public class MainActivity extends AppCompatActivity {
 
     @InjectView(R.id.recycler_view)
-    PullToRefreshRecyclerView mRecyclerView;
-
-    @InjectView(R.id.text_refresh)
-    TextView mText;
+    RecyclerView mRecyclerView;
+    @InjectView(R.id.rotate_header_list_view_frame)
+    PtrClassicFrameLayout mPtrFrame;
 
     private MainAdapter mAdapter;
 
@@ -29,14 +32,35 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.inject(this);
 
         setUpRecyclerView();
+        initPtrFrame();
+    }
 
+    private void initPtrFrame() {
+
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+//               updateData();
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
+        mPtrFrame.setResistance(1.7f);
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(1.2f);
+        mPtrFrame.setDurationToClose(200);
+        mPtrFrame.setDurationToCloseHeader(1000);
+        mPtrFrame.setPullToRefresh(false);
+        mPtrFrame.setKeepHeaderWhenRefresh(true);
     }
 
     private void setUpRecyclerView() {
         mAdapter = new MainAdapter(this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setScrollType(PullToRefreshRecyclerView.OverScrollType.TOP);
-        mRecyclerView.setTextView(mText);
         ArrayList<String> datas = createTestDatas();
         mAdapter.updateData(datas);
     }
@@ -48,6 +72,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return datas;
     }
-
 
 }
---
+++ pulltorefreshrecyclerview/src/main/java/com/loopeer/pulltorefreshrecyclerview/view/PtrCustomFrameLayout.java
@@ -0,0 +1,24 @@
+package com.loopeer.pulltorefreshrecyclerview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import in.srain.cube.views.ptr.PtrFrameLayout;
+
+/**
+ * Created by loopeer on 2015/7/10.
+ */
+public class PtrCustomFrameLayout extends PtrFrameLayout {
+
+    public PtrCustomFrameLayout(Context context) {
+        super(context);
+    }
+
+    public PtrCustomFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PtrCustomFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
